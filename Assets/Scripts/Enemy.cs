28ba226f6b106356a1466daba97df56a5dab7102
jpbenge <jpbenge@gmail.com>
2013-12-06 16:06:22 -0500

    blueKratchet enemy dude

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int health = 0;
+	public int maxHealth = 100;
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+			OnDeath();
+	}
+
+	void OnDeath()
+	{
+		Destroy(gameObject);
+	}
+
+	void OnHit(int dmg)
+	{
+		health -= dmg;
+	}
+}
---
+++ Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFactory : MonoBehaviour {
+
+	public Enemy[] enemyArray;
+
+	// Use this for initialization
+	void Start () {
+		InitEnemies();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void InitEnemies()
+	{
+		GameObject[] enemies;
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		enemyArray = new Enemy[enemies.Length];
+		for (int e = 0; e < enemies.Length;e++)
+		{
+			enemyArray[e] = enemies[e].GetComponent<Enemy>();
+		}
+	}
+}
